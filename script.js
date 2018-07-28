ef4722a8e843b1ff907c68f501c1f26496ef8f6f
Meg <megunaokawa@gmail.com>
2018-07-29 00:36:38 +0900

    fixed problems with d3 chart, now fully manipulatable with text input of mean and standard deviation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//calculate Normal PDF given x-value, mean, and standard deviation
 let getNormPdf = function(x,mean,stdev) {
   let variance = stdev * stdev;
   let a = Math.pow((2 * Math.PI * variance),(-1/2));
@@ -5,14 +6,23 @@ let getNormPdf = function(x,mean,stdev) {
   return a * b;
 };
 
+//initialize variables
 let testObj = [];
 let mean=0,stdev=1;
 let lower,upper,inc;
+let width = 960, height = 600, margin = 30;
 
+//generate set of x-values and y-values for a Normal curve
 let genData = function(m,s) {
-  lower = m - (s*3);
-  upper = m + (s*3);
-  inc = (Math.abs(lower) * 2) / 150;
+  testObj.length = 0;
+  lower = m - (s*6);
+  upper = m + (s*6);
+  inc = (upper - lower) / 100;
+  console.log("mean:"+m);
+  console.log("st dev:"+s);
+  console.log("lower: "+lower);
+  console.log("upper: "+upper);
+  console.log("increment: "+inc);
   for (let x=lower; x<upper; x+=inc) {
     testObj.push({
       'xVal':x,
@@ -23,50 +33,98 @@ let genData = function(m,s) {
   console.log(testObj);
 }
 
+//set up main chart
 
-let chart = d3.select('#wrapper')
-  .attr('width','960')
-  .attr('height','600');
+d3.select('#wrapper')
+  .attr('width',width+(margin*2))
+  .attr('height',height+(margin*2))
 
+let chart = d3.select('#wrapper').append('g')
+  .attr('width',width+(margin*2))
+  .attr('height',height+(margin*2))
+  .attr('transform','translate(0,'+margin+')');
 
-let area = d3.area();
-let yScale = d3.scaleLinear();
-let xScale = d3.scaleLinear();
-let normLine = chart.append('path');
+genData(0,1);
+
+let yScale = d3.scaleLinear()
+  .domain([0,d3.max(testObj,function(d){return +d.normPdf + 0.1})])
+  .range([height,0]);
+
+let xScale = d3.scaleLinear()
+  .domain([lower,upper])
+  .range([0,width]);
+
+let xAxis = d3.axisBottom(xScale)
+  .tickArguments([9]);
+
+let yAxis = d3.axisLeft(yScale);
+
+let area = d3.area()
+  .x(function(d){return xScale(+d.xVal)})
+  .y1(function(d){return yScale(+d.normPdf)})
+  .y0(yScale(0));
+
+let normLine = chart.append('path')
+  .datum(testObj)
+  .attr('fill','purple')
+  .attr("class", "norm-line")
+  .attr('transform','translate('+margin+','+-margin+')')
+  .attr('d',area);
+
+chart.append('g')
+  .attr("class", "x-axis axis")
+  .attr('transform','translate('+margin+','+(height-margin)+')')
+  .call(xAxis);
+
+chart.append('g')
+  .attr("class", "y-axis axis")
+  .attr('transform','translate('+margin+','+-margin+')')
+  .call(yAxis);
 
 
 let updateCurve = function() {
-  mean = Number(d3.select("#input-mean").property("value"));
-	stdev = Number(d3.select("#input-stdev").property("value"));
 
-  let q = d3.queue();
-  q.defer(genData(mean,stdev));
-  q.await(function() {
-    xScale
-      .domain([lower,upper])
-      .range([0,960]);
+  console.log(mean, stdev);
+  console.log(d3.select("#input-mean").property("value"));
+  console.log(typeof d3.select("#input-mean").property("value"));
+  if (parseInt(d3.select("#input-mean").property("value")) !== NaN) {
+    mean = Number(d3.select("#input-mean").property("value"))
+  }
+  if (parseInt(d3.select("#input-stdev").property("value")) !== NaN ) {
+    stdev = Number(d3.select("#input-stdev").property("value"))
+  }
+  console.log(mean, stdev);
 
-    yScale
-      .domain([0,d3.max(testObj,function(d){return d.normPdf})])
-      .range([600,0]);
+  genData(mean,stdev);
 
-    area
-      .x(function(d){return xScale(+d.xVal)})
-      .y1(function(d){return yScale(+d.normPdf)})
-      .y0(yScale(0));
+  xScale
+    .domain([lower,upper]);
 
-    normLine.enter()
-      .datum(testObj)
-      .attr('fill','pink')
-      .attr('d',area);
-  });
+  yScale
+  .domain([0,d3.max(testObj,function(d){return +d.normPdf + 0.1})]);
+
+  area
+    .x(function(d){return xScale(+d.xVal)})
+    .y1(function(d){return yScale(+d.normPdf)})
+    .y0(yScale(0));
+
+  normLine
+    .datum(testObj)
+    .attr('d',area);
+
+  let t = chart.transition()
+    .duration(600);
+
+  t.select('.norm-line')
+    .attr('d',area);
+
+  t.select('.x-axis')
+    .call(xAxis);
+
+  t.select('.y-axis')
+    .call(yAxis);
 
 };
-
-updateCurve();
-
-let xAxis = d3.axisBottom();
-let yAxis = d3.axisLeft();
 
 
 d3.selectAll(".input-stat").on("input", function(){

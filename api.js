d41004cae5a2ea863efc85d77b70c140071d37fa
Meg <megunaokawa@gmail.com>
2018-07-28 20:58:12 +0900

    set up basic frame for grabbing test statistic and p-value from the python script

--- api.js
+++ api.js
@@ -1,31 +1,28 @@
 "use strict";
 
 let spawn = require('child_process').spawn;
-
-let py = spawn('python',['script.py']);
-
-let data = [5,10,.168,'two-sided'];
-let dataString = '';
-
-py.stdout.on('data', function(data){
-  dataString += data.toString();
-  // console.log('data!!',data,dataString);
-
+let express = require('express'), app = express();
+var server = app.listen(3005,  "127.0.0.1", function () {
 });
-py.stdout.on('end', function(){
-  console.log('FINAL ANSWER:');
-  console.log(dataString);
-});
-py.stdin.write(JSON.stringify(data));
-py.stdin.end();
 
-//
-//
-//
-//
-// let getNormPdf = function(x,mean,stdev) {
-//   let variance = stdev * stdev;
-//   let a = Math.pow((2 * Math.PI * variance),(-1/2));
-//   let b = Math.exp(Math.pow((x-mean),2)/(-2*variance));
-//   return a * b;
-// };
+let get_py(stats) {
+  let py = spawn('python',['script.py']);
+
+  // let data = [5,10,.168,'two-sided'];
+  let data = stats;
+  let dataString = '';
+
+  py.stdout.on('data', function(data){
+    dataString += stats.toString();
+    // console.log('data!!',data,dataString);
+
+  });
+  py.stdout.on('end', function(){
+    console.log('FINAL ANSWER:');
+    console.log(dataString);
+  });
+  py.stdin.write(JSON.stringify(stats));
+  py.stdin.end();
+}
+
+app.get('/api/:type')

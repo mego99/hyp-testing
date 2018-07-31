c55766e9bf0b030d3caa05719e8bfdac1e8bb62f
Meg <megunaokawa@gmail.com>
2018-07-31 20:05:53 +0900

    added functionality for two prop z test / 1 mean t test / 2 mean t test on both the python side and the api

--- api.js
+++ api.js
@@ -18,28 +18,45 @@ let get_py = function(req,res,next) {
       res.locals.data = str;
       next();
     });
+    req.params.type = res.locals.testType;
     console.log(JSON.stringify(req.params));
+    console.log(res.locals.testType);
     py.stdin.write(JSON.stringify(req.params));
     py.stdin.end();
 };
 
 
-app.get('/api/z1/:suc-:n-:prop-:alt', [get_py], function(req,res) {
+app.get('/api/z1/:suc-:n-:prop-:alt', function(req,res,next) {
+  res.locals.testType = 'z1';
+  next();
+}, [get_py], function(req,res) {
   res.send(res.locals.data);
 });
 
-app.get('/api/z2/:suc1-:suc2-:n1-:n2-:alt', function(req,res) {
-  res.send(req.params);
+app.get('/api/z2/:suc1-:suc2-:n1-:n2-:alt', function(req,res,next) {
+  res.locals.testType = 'z2';
+  next();
+}, [get_py], function(req,res) {
+  res.send(res.locals.data);
 });
 
-app.get('/api/t1/:mu-:xbar-:xsd-:n-:alt', function(req,res) {
-  res.send(req.params);
+app.get('/api/t1/:mu-:xbar-:xsd-:n-:alt', function(req,res,next) {
+  res.locals.testType = 't1';
+  next();
+}, [get_py], function(req,res) {
+  res.send(res.locals.data);
 });
 
-app.get('/api/t2/:xbar1-:xsd1-:n1-:xbar2-:xsd2-:n2-:alt', function(req,res) {
-  res.send(req.params);
+app.get('/api/t2/:xbar1-:xsd1-:n1-:xbar2-:xsd2-:n2-:alt', function(req,res,next) {
+  res.locals.testType = 't2';
+  next();
+}, [get_py], function(req,res) {
+  res.send(res.locals.data);
 });
 
-app.get('/api/temp/:mu-:xbar-:xsd-:n-:alt', function(req,res) {
-  res.send(req.params);
+app.get('/api/temp/:mu-:xbar-:xsd-:n-:alt', function(req,res,next) {
+  res.locals.testType = 'tm';
+  next();
+}, [get_py], function(req,res) {
+  res.send(res.locals.data);
 });
--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from scipy.stats import norm, chi2_contingency
+from scipy import stats as sp
 import statsmodels.api as sm
 import numpy as np
 import sys, json
@@ -23,16 +23,47 @@ def get_params():
 
 
 def main():
+
+
     # print(get_params())
     # sys.stdout.flush()
     # str = "{'suc': '5', 'n': '10', 'prop': '0.168', 'alt': 'larger'}"
     params = json.loads(get_params(), object_hook=to_float)
-    # print(params)
     # sys.stdout.flush()
     # response.append(params)
-    z1, p_value1 = sm.stats.proportions_ztest(params['suc'],params['n'],params['prop'],params['alt'],params['prop'])
-    response['teststat'] = z1
-    response['pval'] = p_value1
+    type = params['type']
+
+    if (type == 'z1'):
+        z1, p_value1 = sm.stats.proportions_ztest(params['suc'],params['n'],params['prop'],params['alt'],params['prop'])
+        response['teststat'] = z1
+        response['pval'] = p_value1
+    elif (type == 'z2'):
+        prop = (params['suc1'] + params['suc2']) / (params['n1'] + params['n2'])
+        z1, p_value1 = sm.stats.proportions_ztest([params['suc1'],params['suc2']],[params['n1'],params['n2']],0,params['alt'],prop)
+        response['teststat'] = z1
+        response['pval'] = p_value1
+    elif (type == 't1'):
+        tstat, pval = sp.stats.ttest_ind_from_stats(mean1=params['xbar'],
+                                      std1=params['xsd'],
+                                      nobs1=params['n'],
+                                      mean2=params['mu'],
+                                      std2=0,
+                                      nobs2=2,
+                                      equal_var=False)
+        response['teststat'] = tstat
+        response['pval'] = pval / 2
+    elif (type == 't2'):
+        tstat, pval = sp.stats.ttest_ind_from_stats(mean1=params['xbar1'],
+                                      std1=params['xsd1'],
+                                      nobs1=params['n1'],
+                                      mean2=params['xbar2'],
+                                      std2=params['xsd2'],
+                                      nobs2=['n2'],
+                                      equal_var=False)
+        response['teststat'] = tstat
+        response['pval'] = pval / 2
+
+
     print(json.dumps(response))
     sys.stdout.flush()
 
